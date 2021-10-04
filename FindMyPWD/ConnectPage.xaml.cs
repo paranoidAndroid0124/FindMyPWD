bc7e043734188b6f28623650d443dcef80ddc76c
paranoidAndroid0124 <vincentgagnon@cmail.carleton.ca>
2021-10-04 14:34:22 -0400

    Started to add bluetooth scanning

--- FindMyPWD/ConnectPage.xaml.cs
+++ FindMyPWD/ConnectPage.xaml.cs
@@ -1,5 +1,8 @@
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +15,24 @@ namespace FindMyPLWD
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConnectPage : ContentPage
     {
+        IBluetoothLE ble;
+        IAdapter adapter;
+        ObservableCollection<IDevice> deviceList;
+        IDevice device;
         public ConnectPage()
         {
             InitializeComponent();
+            ble = CrossBluetoothLE.Current;
         }
         void Select(object sender, System.EventArgs e)
         {
             TempLbl.Text = ((Button)sender).Text;
             //send to setupscan if not setup or temp, else just scan
+        }
+        async void ScanBLE(object sender, System.EventArgs e)
+        {
+            adapter.DeviceDiscovered += (s, a) => deviceList.Add(a.Device);
+            await adapter.StartScanningForDevicesAsync();
         }
     }
 }
