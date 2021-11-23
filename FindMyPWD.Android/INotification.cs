cb6151400410cf00a957e9ca78726fa5b786cf91
paranoidAndroid0124 <vincentgagnon@cmail.carleton.ca>
2021-11-23 09:27:33 -0500

    trying to figure out how to set up a periodic scanning service

---
+++ FindMyPWD.Android/INotification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMyPWD.Interface
+{
+    public interface INotification
+    {
+        Notification ReturnNotif();
+    }
+}
---
+++ FindMyPWD/Interface/IAndroidService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMyPWD.Interface
+{
+    public interface IAndroidService
+    {
+        void StartService();
+
+        void StopService();
+    }
+}
